2018b513f4779a5a2732d44f453b4939289643f4
lgionrise <lgionrise@gmail.com>
2026-09-26 08:53:52 +0530

    Add BrevoEmailProvider to OtpModule providers

--- src/modules/otp/otp.module.ts
+++ src/modules/otp/otp.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { OtpService } from './otp.service';
+import { BrevoEmailProvider } from './providers/brevo-email.provider';
 import { ConsoleEmailProvider } from './providers/console-email.provider';
 import { ConsoleSmsProvider } from './providers/console-sms.provider';
 
 @Module({
-  providers: [OtpService, ConsoleEmailProvider, ConsoleSmsProvider],
+  providers: [OtpService, BrevoEmailProvider, ConsoleEmailProvider, ConsoleSmsProvider],
   exports: [OtpService],
 })
 export class OtpModule {}

6e2889ace6af64816974b172715d838a3398f4af
lgionrise <lgionrise@gmail.com>
2026-09-26 08:58:49 +0530

    Add Dashboard service and controller to StudentModule

--- src/modules/student/student.module.ts
+++ src/modules/student/student.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { StudentService } from './student.service';
 import { StudentController } from './student.controller';
+import { DashboardService } from './dashboard.service';
+import { DashboardController } from './dashboard.controller';
 
 @Module({
-  controllers: [StudentController],
-  providers: [StudentService],
-  exports: [StudentService],
+  controllers: [StudentController, DashboardController],
+  providers: [StudentService, DashboardService],
+  exports: [StudentService, DashboardService],
 })
 export class StudentModule {}

707df1757d4da58fc2867acf229c776363007852
lgionrise <lgionrise@gmail.com>
2026-09-26 11:52:31 +0530

    Create TeacherModule with controllers and service

---
+++ src/modules/teacher/teacher.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { TeacherService } from './teacher.service';
+import { TeacherController } from './teacher.controller';
+import { TeacherAdminController } from './teacher-admin.controller';
+
+@Module({
+  controllers: [TeacherController, TeacherAdminController],
+  providers: [TeacherService],
+  exports: [TeacherService],
+})
+export class TeacherModule {}
